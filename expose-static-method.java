0a22e91098c757b91d8ebbe87933e5acb3f2f2bf
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-08 09:31:14 +0700

    Make static

--- expose-static-method.java
+++ expose-static-method.java
@@ -1,5 +1,9 @@
 class RSCWorkflow {
   public void validate(Packet packet) throws InvalidFlowException {
+    validatePacket(packet);
+  }
+
+  public static void validatePacket(Packet packet) throws InvalidFlowException {
     if (packet.getOriginator().equals("MIA") {
         || packet.getLength() > MAX_LENGTH
         || !packet.hasValidCheckSum()) {
