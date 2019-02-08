c8b9a3514835705636ee971b7ef875ef4a57b667
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-08 10:19:10 +0700

    WIP create iface

--- extract-interface.java
+++ extract-interface.java
@@ -8,6 +8,33 @@ void testPayday() {
       getTestingDatabase().findCheck(12));
 }
 
+public class PaydayTransaction extends Transaction {
+  public PaydayTransaction(PayrollDatabase db, TransactionRecorder log) {
+    super(db, log);
+  }
+
+  public void run() {
+    for(Iterator it = db.getEmployees(); it.hasNext()) {
+      Employee e = (Employee)it.next();
+      if (e.isPayday(date)) {
+        e.pay();
+      }
+    }
+    log.saveTransaction(this);
+  }
+}
+
+interface TransactionRecorder {
+}
+
+public class TransactionLog implements TransactionRecorder {
+  // ...
+}
+
+public class FakeTransactionLog implements TransactionRecorder {
+  // ...
+}
+
 // FakeTransactionLog doesn't exist yet
 //   => need to extract an interface for TransactionLog class
 //   & make it possible for PaydayTransaction to accept a FakeTransactionLog
